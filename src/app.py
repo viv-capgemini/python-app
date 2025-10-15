ddbcedbfd1ebd55d70da7668841fb54a1c934702
Viv Ansah <viv.capgemini@gmail.com>
2025-10-15 13:48:12 +0100

    added self-hoster runner

--- src/app.py
+++ src/app.py
@@ -24,7 +24,7 @@ def details():
             'date': date,
             'time': time,
             'hostname': get_hostname(),
-            'message': 'GitHub/DockerHub mixed up'
+            'message': 'GitHub/DockerHub mixed up solved'
         
         })
 
