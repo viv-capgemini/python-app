0e08e09552a4cfbc645fc6a127cc92e00114ef84
Viv Ansah <viv.capgemini@gmail.com>
2025-10-16 09:04:31 +0100

    updated ci job with needs

--- src/app.py
+++ src/app.py
@@ -25,7 +25,7 @@ def details():
             'time': time,
             'hostname': get_hostname(),
             'message': 'GitHub/DockerHub mixed up solved',
-            'travelled': 'Amsterdam Marathon'
+            'travelled': 'Amsterdam Marathon- Day 1'
         
         })
 
